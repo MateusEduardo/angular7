c8f4cfcb978cb651b05e08fbb0751ecd89c24dd0
Mateus Eduardo <mateusdarosadealmeida@gmail.com>
2019-03-18 15:27:56 -0300

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Windowstorm' },
+  { id: 2, name: 'Superman' },
+  { id: 3, name: 'Narco' },
+  { id: 4, name: 'Criptonita' },
+  { id: 5, name: 'Chespirito' },
+  { id: 6, name: 'lanterna verde' },
+  { id: 7, name: 'Chapolin Colorado' },
+  { id: 8, name: 'Batman' },
+  { id: 9, name: 'The Flash' },
+  { id: 10, name: 'Homem de Gelo' },
+  { id: 11, name: 'Homem de Ferro' },
+]
